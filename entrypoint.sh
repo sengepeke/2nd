759cf05ee7f70b1cb468d176678630a0b014277b
sengepeke <89533527+sengepeke@users.noreply.github.com>
2023-03-18 21:34:26 +0700

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,11 +4,11 @@
 if [ "${use_proxy}" = true ]; then
     # check whether proxychains4.conf exists
     if [ -f /root/proxychains4.conf ]; then
-        proxychains4 -q -f /root/proxychains4.conf kintil 
+        proxychains4 -q -f /root/proxychains4.conf kintil cut.ini
     else
         echo "Proxychains4.conf not found, exit"
         exit 1
     fi
 else
-   kintil 
+   kintil cut.ini
 fi
